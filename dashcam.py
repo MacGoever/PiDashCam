ebe305370fc9e250be28aae96df70125a2b57393
MacGoever <macgoever@gmx.li>
2018-04-21 20:35:45 +0000

    First version that starts and writes a gpx file.

--- dashcam.py
+++ dashcam.py
@@ -6,141 +6,49 @@ import threading
 import os
 import math
 from datetime import *
+from libdashcam import *
 
-gpsd=None
-
-class gpsPoller(threading.Thread):
-    def __init__(self):
-        threading.Thread.__init__(self)
-        global gpsd
-        gpsd = gps(mode=WATCH_ENABLE)
-        self.current_value=None
-        self.running = True
-
-    def run(self):
-        global gpsd
-        while self.running:
-            gpsd.next()
-
-class gpxWriter(threading.Thread):
-    def __init__(self, path,minDist,pollDelay):
-        threading.Thread.__init__(self)
-        self.oldLon=0
-        self.oldLat=0
-        self.path=path
-        self.pollDelay=pollDelay
-        self.minDist=minDist
-
-    def writeWaypoint(self):
-        global gpsd
-
-#   <trkpt lat="51.931957" lon="7.622663">
-#    <ele>79.300000</ele>
-#       <time>2018-04-04T13:31:12.000Z</time>
-#           <fix>2d</fix>
-#               <sat>4</sat>
-#                   <hdop>2.3</hdop>
-#                       <vdop>1.0</vdop>
-#                           <pdop>2.5</pdop>
-#                              </trkpt>
-
-
-    def run(self):
-        deglen=110.25
-        global gpsd
-        with open(self.path, "w") as gpx_log:
-                gpx_log.write( "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n")
-		gpx_log.write("<gpx version=\"1.1\" creator=\"dashcam.py\"\n")
-		gpx_log.write("     xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n")
-		gpx_log.write("xmlns=\"http://www.topografix.com/GPX/1/1\"\n")
-		gpx_log.write("        xsi:schemaLocation=\"http://www.topografix.com/GPX/1/1\n")
-		gpx_log.write("        http://www.topografix.com/GPX/1/1/gpx.xsd\">\n")
-		gpx_log.write(" <metadata>\n")
-		gpx_log.write("  <time>{0}</time>\n".format(gpsd.utc))
-		gpx_log.write(" </metadata>\n")
-		gpx_log.write(" <trk>\n")
-		gpx_log.write("  <src>GPSD %s</src>\n", VERSION)
-		gpx_log.write("  <trkseg>\n")
-
-
-
-        #write headers - timestamp
-        writeWaypoint()
-
-        self.oldLon=gpsd.fix.longitude
-        self.oldLat=gpsd.fix.latitude
-        
-        while self.running:
-            #simplified distance calculation. Only suitable for distances < 5km 
-            x = gpsd.fix.latitude - self.oldLat
-            y = (gpsd.fix.longitude - self.oldLon) * math.cos(self.oldLat)
-            distanceTraveled=deglen*math.sqrt(x*x + y*y) * 1000
-
-            if (distanceTraveled > self.minDist):
-                writeWaypoint()
-
-            self.oldLon=gpsd.fix.longitude
-            self.oldLat=gpsd.fix.latitude
-            
-            sleep(self.pollDelay)
-
-
-
-#"  </trkseg>\n");
-#" </trk>\n");
-#"</gpx>\n");
-
-
-
+gpsp =  None
 
 if __name__ == '__main__':
     gpsp = gpsPoller()
     gpsp.start()
 
-    localTime = datetime.utcnow()
-    gpsTime = None
-    timeDiff=timedelta(seconds=10)
+    gpxp = gpxWriter(gpsp, "bla.gpx", 1 , 5)
+    gpxp.start()
 
     try:
-
-        
-        while gpsd.fix.mode == MODE_NO_FIX or timeDiff.seconds == 0:
-            print ("Waiting for fix and Timsync")
-            try:    #In case no fix is present, the parsing would fail
-                gpsTime = datetime.strptime(gpsd.utc, "%Y-%m-%dT%H:%M:%S.000Z")
-                localTime = datetime.utcnow()
-                timeDiff= localTime - gpsTime
-            except:
-                pass
-
-            sleep(2)
-
-
-
-
         while 1:
-            print
-            print ' GPS reading'
-            print '----------------------------------------'
-            print 'latitude    ' , gpsd.fix.latitude
-            print 'longitude   ' , gpsd.fix.longitude
-            print 'time utc    ' , gpsd.utc,' + ', gpsd.fix.time
-            print 'altitude (m)' , gpsd.fix.altitude
-            print 'eps         ' , gpsd.fix.eps
-            print 'epx         ' , gpsd.fix.epx
-            print 'epv         ' , gpsd.fix.epv
-            print 'ept         ' , gpsd.fix.ept
-            print 'speed (m/s) ' , gpsd.fix.speed
-            print 'climb       ' , gpsd.fix.climb
-            print 'track       ' , gpsd.fix.track
-            print 'mode        ' , gpsd.fix.mode
-            print
-            print 'sats        ' , gpsd.satellites
+            try:
+                fix=gpsp.getFix()
+                utc=gpsp.getUTC()
+                satellites=gpsp.getSatellites()
+                print
+                print ' GPS reading'
+                print '----------------------------------------'
+                print 'latitude    ' , fix.latitude
+                print 'longitude   ' , fix.longitude
+                print 'time utc    ' , utc,' + ', fix.time
+                print 'altitude (m)' , fix.altitude
+                print 'eps         ' , fix.eps
+                print 'epx         ' , fix.epx
+                print 'epv         ' , fix.epv
+                print 'ept         ' , fix.ept
+                print 'speed (m/s) ' , fix.speed
+                print 'climb       ' , fix.climb
+                print 'track       ' , fix.track
+                print 'mode        ' , fix.mode
+                print 'sats        ' , gpsp.getNumberOfSatellites()
+
+            except (IOError):
+                pass
     
             sleep(1) #set to whatever
 
     except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
         print "\nKilling Thread..."
+        gpxp.running = False
+        gpxp.join()
         gpsp.running = False
         gpsp.join() # wait for the thread to finish what it's doing
 print "Done.\nExiting."
--- libdashcam.py
+++ libdashcam.py
@@ -24,49 +24,78 @@ class gpsPoller(threading.Thread):
         
     def getFix(self):
         if self.getFixPresent():
-            return self.gpsd.fix
+            fix=self.gpsd.fix
+            fix.speed=fix.speed*0.514444 #from knots to m/s
+            return fix
         else:
             raise IOError("No GPS fix")
+
+    def getFixWait(self):
+        while not (self.getFixPresent()):
+            sleep(.5) 
+        fix=self.gpsd.fix
+        fix.speed=fix.speed*0.514444 #from knots to m/s
+        return fix
 
     def getUTC(self):
         if self.getFixPresent():
-            return gpsd.UTC
+            return self.gpsd.utc
         else:
             raise IOError("No GPS fix")
 
-    def getSatelites(self):
-        if self.getFixPresent():
-            return self.gpsd.satelites
-        else:
-            raise IOError("No GPS fix")
+    def getUTCWait(self):
+        while not (self.getFixPresent()):
+            sleep(.5) 
+        return self.gpsd.utc
+
+    def getSatellites(self):
+            return self.gpsd.satellites
+
+    def getNumberOfSatellites(self):
+            return len(self.gpsd.satellites)
+
+    def getDistanceTravelled(self,oldLat,oldLon):
+        deglen=110.25
+        try:
+            fix=self.getFix()
+
+            #simplified distance calculation. Only suitable for distances < 5km 
+            x = fix.latitude - oldLat
+            y = (fix.longitude - oldLon) * math.cos(oldLat)
+            distanceTravelled=deglen*math.sqrt(x*x + y*y) * 1000
+            return distanceTravelled
+        except (IOError):
+            return 0
+
+
+
 
 class gpxWriter(threading.Thread):
-    def __init__(self, path,minDist,pollDelay):
+    def __init__(self,gpsp, path,minDist,pollDelay):
         threading.Thread.__init__(self)
         self.oldLon=0
         self.oldLat=0
         self.path=path
         self.pollDelay=pollDelay
         self.minDist=minDist
+        self.gpsp=gpsp
+        self.fix=self.gpsp.getFixWait()
+        self.running = True
 
     def writeWaypoint(self):
-        global gpsp
-
-        with open(self.path, "w") as gpx_log:
-		gpx_log.write("<trkpt lat=\"{0}\" lon=\"{1}\">".format(gpsp.getFix().latitude, gpsd.getFix.longitude))
-		gpx_log.write("<ele>{0}</ele>".format(gpsd.altitude))
-		gpx_log.write("<time>2018-04-04T13:31:12.000Z</time>")
-		gpx_log.write("<fix>2d</fix>")
-		gpx_log.write("<sat>4</sat>")
-		gpx_log.write("<hdop>2.3</hdop>")
-		gpx_log.write("<vdop>1.0</vdop>")
-		gpx_log.write("<pdop>2.5</pdop>")
-		gpx_log.write("</trkpt>")
+        with open(self.path, "a") as gpx_log:
+		gpx_log.write("<trkpt lat=\"{0}\" lon=\"{1}\">\n".format(self.fix.latitude, self.fix.longitude))
+		gpx_log.write("<ele>{0}</ele>\n".format(self.fix.altitude))
+		gpx_log.write("<time>2018-04-04T13:31:12.000Z</time>\n")
+		gpx_log.write("<fix>2d</fix>\n")
+		gpx_log.write("<sat>4</sat>\n")
+		gpx_log.write("<hdop>2.3</hdop>\n")
+		gpx_log.write("<vdop>1.0</vdop>\n")
+		gpx_log.write("<pdop>2.5</pdop>\n")
+		gpx_log.write("</trkpt>\n")
 
 
     def run(self):
-        deglen=110.25
-        global gpsd
         with open(self.path, "w") as gpx_log:
                 gpx_log.write( "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n")
 		gpx_log.write("<gpx version=\"1.1\" creator=\"dashcam.py\"\n")
@@ -75,36 +104,29 @@ class gpxWriter(threading.Thread):
 		gpx_log.write("        xsi:schemaLocation=\"http://www.topografix.com/GPX/1/1\n")
 		gpx_log.write("        http://www.topografix.com/GPX/1/1/gpx.xsd\">\n")
 		gpx_log.write(" <metadata>\n")
-		gpx_log.write("  <time>{0}</time>\n".format(gpsd.utc))
+		gpx_log.write("  <time>{0}</time>\n".format(self.gpsp.getUTCWait()))
 		gpx_log.write(" </metadata>\n")
 		gpx_log.write(" <trk>\n")
-		gpx_log.write("  <src>GPSD %s</src>\n", VERSION)
+		gpx_log.write("  <src>GPSD</src>\n")
 		gpx_log.write("  <trkseg>\n")
 
 
-
         #write headers - timestamp
-        writeWaypoint()
-
-        self.oldLon=gpsd.fix.longitude
-        self.oldLat=gpsd.fix.latitude
+        self.writeWaypoint()
+        self.oldLon=self.fix.longitude
+        self.oldLat=self.fix.latitude
         
         while self.running:
-            #simplified distance calculation. Only suitable for distances < 5km 
-            x = gpsd.fix.latitude - self.oldLat
-            y = (gpsd.fix.longitude - self.oldLon) * math.cos(self.oldLat)
-            distanceTraveled=deglen*math.sqrt(x*x + y*y) * 1000
 
-            if (distanceTraveled > self.minDist):
-                writeWaypoint()
-
-            self.oldLon=gpsd.fix.longitude
-            self.oldLat=gpsd.fix.latitude
+            if (self.gpsp.getDistanceTravelled(self.oldLat,self.oldLon) > self.minDist):
+                fix=gpsp.getFixWait()
+                self.oldLon=fix.longitude
+                self.oldLat=fix.latitude
+                self.writeWaypoint() 
             
             sleep(self.pollDelay)
 
-
-
-#"  </trkseg>\n");
-#" </trk>\n");
-#"</gpx>\n");
+        with open(self.path, "a") as gpx_log:
+                gpx_log.write("</trkseg>\n");
+                gpx_log.write("</trk>\n");
+                gpx_log.write("</gpx>\n");
