057c9a9590b5c43c5cc1225896bbe5aaafb952a8
MacGoever <macgoever@gmx.li>
2018-05-26 18:46:59 +0000

    Added Camera Thread
    Altered Thread Termination to use Event Object

--- libdashcam.py
+++ libdashcam.py
@@ -6,6 +6,42 @@ import threading
 import os
 import math
 from datetime import *
+from picamera import PiCamera
+
+class photoTaker(threading.Thread):
+    def __init__(self, gpsp, interval, path, active, callBack):
+        threading.Thread.__init__(self)
+        self.gpsp = gpsp
+        self.interval=interval
+        self.path=path
+        self.active = active
+        self.callBack=callBack
+        self.camera=PiCamera()
+        self.camera.rotation = 180
+        self.camera.resolution = (1920,1080)
+        self.exit=threading.Event()
+
+    def run (self):
+        while not self.exit.is_set():
+            if self.active:
+                filename=self.path+datetime.strftime(datetime.utcnow(),"%Y-%m-%d_%H-%M-%S.jpg")
+                self.camera.capture(filename)
+                self.addEXIF(filename)
+                self.callBack()
+
+            self.exit.wait(self.interval)
+
+    def stop (self):
+        self.exit.set()
+
+    def instantImage(self,channel):
+        filename=self.path+"insta"+datetime.strftime(datetime.utcnow(),"%Y-%m-%d_%H-%M-%S.jpg")
+        self.camera.capture(filename)
+        self.addEXIF(filename)
+        self.callBack()
+
+    def addEXIF(self,filename):
+        print("adding EXIF")
 
 class gpsPoller(threading.Thread):
     def __init__(self):
@@ -14,63 +50,47 @@ class gpsPoller(threading.Thread):
         self.current_value=None
         self.running = True
         self.fixAge=0
+        self.exit=threading.Event()
 
     def run(self):
-        while self.running:
+        while not self.exit.is_set():
             self.gpsd.next()
             if (self.gpsd.fix.mode == MODE_NO_FIX): 
                 self.fixAge = self.fixAge + 1
             else:
                 self.fixAge=0
-            sleep(1)
+            self.exit.wait(0.9)
 
     def getFixPresent(self):
-        return (self.fixAge == 0)
+        return (not self.gpsd.fix.mode == MODE_NO_FIX)
         
     def getFix(self):
         if self.getFixPresent():
-            fix=self.gpsd.fix
-            fix.speed=fix.speed*0.514444 #from knots to m/s
-            return fix
+            return self.gpsd.fix, self.gpsd.utc
         else:
-            raise IOError("No GPS fix")
-
-    def getFixWait(self):
-        while not (self.getFixPresent()):
-            sleep(.5) 
-        fix=self.gpsd.fix
-        fix.speed=fix.speed*0.514444 #from knots to m/s
-        return fix
-
-    def getUTC(self):
-        if self.getFixPresent():
-            return self.gpsd.utc
-        else:
-            raise IOError("No GPS fix")
-
-    def getUTCWait(self):
-        while not (self.getFixPresent()):
-            sleep(.5) 
-        return self.gpsd.utc
+            return None, None
 
     def getSatellites(self):
             return self.gpsd.satellites
+
+    def stop (self):
+        self.exit.set()
 
     def getNumberOfSatellites(self):
             return len(self.gpsd.satellites)
 
     def getDistanceTravelled(self,oldLat,oldLon):
         deglen=110.25
-        try:
-            fix=self.getFix()
 
+        fix,utc=self.getFix()
+        if fix == None:
+            return 0
+        else:
             #simplified distance calculation. Only suitable for distances < 5km 
             x = fix.latitude - oldLat
             y = (fix.longitude - oldLon) * math.cos(oldLat)
             distanceTravelled=deglen*math.sqrt(x*x + y*y) * 1000
             return distanceTravelled
-        except (IOError):
-            return 0
 
 class gpxWriter(threading.Thread):
     def __init__(self,gpsp, path,minDist,pollDelay,maxFixAge):
@@ -81,26 +101,31 @@ class gpxWriter(threading.Thread):
         self.pollDelay=pollDelay
         self.minDist=minDist
         self.gpsp=gpsp
-        self.fix=self.gpsp.getFixWait()
-        self.running = True
         self.maxFixAge = maxFixAge
+        self.utc=None
+        self.fix=None
+        self.exit=threading.Event()
 
     def writeWaypoint(self):
         with open(self.path, "a") as gpx_log:
 		gpx_log.write("     <trkpt lat=\"{0}\" lon=\"{1}\">\n".format(self.fix.latitude, self.fix.longitude))
-		gpx_log.write("         <ele>{0}</ele>\n".format(self.fix.altitude))
-		gpx_log.write("         <time>{0}</time>\n".format(self.gpsp.getUTCWait()))
-		gpx_log.write("         <fix>{0}</fix>\n".format(self.fix.mode))
+		gpx_log.write("         <time>{0}</time>\n".format(self.utc))
+                if (self.fix.mode == MODE_2D ):
+		    gpx_log.write("         <fix>2d</fix>\n")
+                elif (self.fix.mode == MODE_3D ):
+		    gpx_log.write("         <fix>3d</fix>\n")
+		    gpx_log.write("         <ele>{0}</ele>\n".format(self.fix.altitude))
 		gpx_log.write("         <sat>{0}</sat>\n".format(self.gpsp.getNumberOfSatellites()))
-                #These need to be calculated...
-		#gpx_log.write("<hdop>{0}</hdop>\n".format(fix.))
-		#gpx_log.write("<vdop>{0}</vdop>\n".format(fix.))
-		#gpx_log.write("<pdop>{0}</pdop>\n".format(fix.))
 		gpx_log.write("     </trkpt>\n")
 
 
     def run(self):
-        with open(self.path, "w") as gpx_log:
+        while (not self.exit.is_set() and (self.fix==None or self.utc==None) ):
+            self.fix, self.utc=self.gpsp.getFix()
+            self.exit.wait(self.pollDelay) 
+
+        if (not self.exit.is_set()):
+            with open(self.path, "w") as gpx_log:
                 gpx_log.write("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n")
 		gpx_log.write(" <gpx version=\"1.1\" creator=\"dashcam.py\"\n")
 		gpx_log.write("     xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n")
@@ -108,34 +133,36 @@ class gpxWriter(threading.Thread):
 		gpx_log.write("     xsi:schemaLocation=\"http://www.topografix.com/GPX/1/1\n")
 		gpx_log.write("     http://www.topografix.com/GPX/1/1/gpx.xsd\">\n")
 		gpx_log.write(" <metadata>\n")
-		gpx_log.write("     <time>{0}</time>\n".format(self.gpsp.getUTCWait()))
+		gpx_log.write("     <time>{0}</time>\n".format(self.utc))
 		gpx_log.write(" </metadata>\n")
 		gpx_log.write(" <trk>\n")
 		gpx_log.write("     <src>GPSD</src>\n")
 		gpx_log.write("     <trkseg>\n")
 
-
-        #write headers - timestamp
-        self.writeWaypoint()
-        self.oldLon=self.fix.longitude
-        self.oldLat=self.fix.latitude
-        
-        while self.running:
+            #write headers - timestamp
+            self.writeWaypoint()
+            self.oldLon=self.fix.longitude
+            self.oldLat=self.fix.latitude
+       
+        while not self.exit.is_set():
 
             if (self.gpsp.getDistanceTravelled(self.oldLat,self.oldLon) > self.minDist):
-                fix=self.gpsp.getFixWait()
-                self.oldLon=fix.longitude
-                self.oldLat=fix.latitude
-                self.writeWaypoint() 
+                self.fix, self.utc=self.gpsp.getFix()
+                if (not self.fix==None):
+                    self.oldLon=self.fix.longitude
+                    self.oldLat=self.fix.latitude
+                    self.writeWaypoint() 
 
             if (self.gpsp.fixAge > self.maxFixAge):
                 with open(self.path, "a") as gpx_log:
 		    gpx_log.write("     </trkseg>\n")
 		    gpx_log.write("     <trkseg>\n")
-            
-            sleep(self.pollDelay)
+            self.exit.wait(self.pollDelay)
 
         with open(self.path, "a") as gpx_log:
                 gpx_log.write("     </trkseg>\n");
                 gpx_log.write("     </trk>\n");
                 gpx_log.write("     </gpx>\n");
+
+    def stop (self):
+        self.exit.set()
